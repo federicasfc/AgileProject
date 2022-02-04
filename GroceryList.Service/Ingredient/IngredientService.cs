fedf130053d99722718e1232d895c82bac5e3770
michaellinn48 <mlinn@fakeemail.com>
2022-02-04 17:08:00 -0500

    Completed all parts of CreateIngredient. Also completed all parts of GetAllIngredients. Accidentally done on the same branch as CreateIngredient.

--- GroceryList.Service/Ingredient/IngredientService.cs
+++ GroceryList.Service/Ingredient/IngredientService.cs
@@ -2,11 +2,52 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using GroceryList.Data;
+using GroceryList.Data.Entities;
+using GroceryList.Models.Ingredient;
+using Microsoft.EntityFrameworkCore;
 
 namespace GroceryList.Service.Ingredient
 {
     public class IngredientService : IIngredientService
     {
-        //Will Contain Ingredient CRUD
+        private readonly ApplicationDbContext _dbContext;
+
+        //Constructor
+        public IngredientService(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+
+        /* CRUD */
+
+        // CreateIngredient method
+        public async Task<bool> CreateIngredientAsync(IngredientCreate request)
+        {
+
+            var ingredientEntity = new IngredientEntity
+            {
+                Name = request.Name,
+                FoodCategory = request.FoodCategory,
+            };
+
+            _dbContext.Ingredients.Add(ingredientEntity);
+
+            var numberOfChanges = await _dbContext.SaveChangesAsync();
+            return numberOfChanges == 1;
+        }
+
+        // GetAllIngredients method
+        public async Task<IEnumerable<IngredientListItem>> GetAllIngredientsAsync()
+        {
+            var ingredients = await _dbContext.Ingredients
+                .Select(entity => new IngredientListItem
+                {
+                    Id = entity.Id,
+                    Name = entity.Name
+                }).ToListAsync();
+            return ingredients;
+        }
     }
 }
--- GroceryList.WebAPI/Controllers/IngredientController.cs
+++ GroceryList.WebAPI/Controllers/IngredientController.cs
@@ -3,14 +3,48 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using GroceryList.Models.Ingredient;
+using GroceryList.Service.Ingredient;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
 namespace GroceryList.WebAPI.Controllers
 {
-    [Route("[controller]")]
+    [Route("api/[controller]")]
+    [ApiController]
     public class IngredientController : ControllerBase
     {
-        //Will contain Http endpoint methods connecting to Service methods
+        // field to access service methods
+        private readonly IIngredientService _ingredientService;
+
+        // contructor
+        public IngredientController(IIngredientService ingredientService)
+        {
+            _ingredientService = ingredientService;
+        }
+
+        //Post 
+        [HttpPost]
+        /* WORKS */
+        public async Task<IActionResult> CreateIngredient([FromForm] IngredientCreate request)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            if (await _ingredientService.CreateIngredientAsync(request) == false)
+                return BadRequest("Ingredient could not be created."); 
+
+
+            return Ok("Ingredient created successfully");
+        }
+
+        // GetAllIngredients endpoint
+        [HttpGet]
+        /* WORKS */
+        public async Task<IActionResult> GetAllIngredients()
+        {
+            var ingredients = await _ingredientService.GetAllIngredientsAsync();
+            return Ok(ingredients);
+        }
     }
 }
